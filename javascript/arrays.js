d03de1345dcc7f2f3aafb3d91966fb02b6ffdbf7
Ben Li <benji112358@gmail.com>
2018-04-27 21:48:42 -0400

    notes

--- javascript/arrays.js
+++ javascript/arrays.js
@@ -6,6 +6,9 @@ let people = [
 	{first:'Perikles',year:494}
 ];
 
+
+//filter for people born in 1500s
+
 function filter() {
 
 	const fifteen = people.filter(person => {
@@ -20,4 +23,62 @@ function filter() {
 	console.table(fifteen)
 }
 
+
+
+//map array for first name
+
+function map() {
+
+	const names = people.map(person => `${person.first}`);
+	console.log(names);
+
+
+}
+
+
+
+// sort people by age
+
+function order() {
+
+	const order = people.sort((a, b) => a.year > b.year ? 1 : -1); 
+		
+		/* above arrow/ternanry syntax is equivalent to below
+		if(a.year > b.year){
+			return 1;
+		} else {
+			return -1;
+		} */
+
+	console.table(order)
+}
+	
+
+// conduct running total with Array.reduce 
+
+function reduce() {
+
+	const totalYears=people.reduce((total, person) => {
+		return total + (person.year);
+	}, 0); //zero is initial value
+	console.log(totalYears);
+
+}
+
+
+
+//take and sort entities from online
+
+function streets() {
+
+	const category = document.querySelector
+
+
+
+}
+
+
 filter();
+map();
+order();
+reduce();
